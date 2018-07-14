ce415958962e2e9b4382fe9b50c0e8f644a4dc38
vinson <vinsonhwj@qq.com>
2018-07-15 00:08:00 +0800

    Add TestCase

---
+++ UnitTest/CommonLibrary/UnitTest1.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using hwj.CommonLibrary;
+
+namespace UnitTest.CommonLibrary
+{
+    [TestClass]
+    public class Test_Security
+    {
+        [TestMethod]
+        public void TestEncrypt()
+        {
+            Assert.IsTrue(hwj.CommonLibrary.Object.SecurityHelper.Encrypt("111111", "VINSON") == "C2BC72DB1918C721");
+        }
+
+        [TestMethod]
+        public void TestDecrypt()
+        {
+            Assert.IsTrue(hwj.CommonLibrary.Object.SecurityHelper.Decrypt("C2BC72DB1918C721", "VINSON") == "111111");
+        }
+    }
+}
---
+++ UnitTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("UnitTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("UnitTest")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("54f129cd-e330-4f5a-b487-7cd227d18318")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
